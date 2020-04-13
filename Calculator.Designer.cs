d40519455b217879d186ecb0f7553aa8632ee055
DianaBanana <dianaskybin@gmail.com>
2020-04-12 22:15:47 -0400

    DoneButtons

---
+++ Calculator.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace _200421248A2
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemoryClear = new System.Windows.Forms.Button();
+            this.MemoryRecall = new System.Windows.Forms.Button();
+            this.MemoryPlus = new System.Windows.Forms.Button();
+            this.MemoryStore = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.number1 = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.ChangeArithmeticSign = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number8 = new System.Windows.Forms.Button();
+            this.Dot = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.Equals = new System.Windows.Forms.Button();
+            this.OneDividedByCurrentEntry = new System.Windows.Forms.Button();
+            this.SquareRoot = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.MemoryDispl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MemoryClear
+            // 
+            this.MemoryClear.Location = new System.Drawing.Point(43, 186);
+            this.MemoryClear.Name = "MemoryClear";
+            this.MemoryClear.Size = new System.Drawing.Size(83, 69);
+            this.MemoryClear.TabIndex = 0;
+            this.MemoryClear.Text = "MC";
+            this.MemoryClear.UseVisualStyleBackColor = true;
+            // 
+            // MemoryRecall
+            // 
+            this.MemoryRecall.Location = new System.Drawing.Point(43, 270);
+            this.MemoryRecall.Name = "MemoryRecall";
+            this.MemoryRecall.Size = new System.Drawing.Size(83, 70);
+            this.MemoryRecall.TabIndex = 1;
+            this.MemoryRecall.Text = "MR";
+            this.MemoryRecall.UseVisualStyleBackColor = true;
+            // 
+            // MemoryPlus
+            // 
+            this.MemoryPlus.Location = new System.Drawing.Point(43, 438);
+            this.MemoryPlus.Name = "MemoryPlus";
+            this.MemoryPlus.Size = new System.Drawing.Size(83, 70);
+            this.MemoryPlus.TabIndex = 3;
+            this.MemoryPlus.Text = "M+";
+            this.MemoryPlus.UseVisualStyleBackColor = true;
+            // 
+            // MemoryStore
+            // 
+            this.MemoryStore.Location = new System.Drawing.Point(43, 353);
+            this.MemoryStore.Name = "MemoryStore";
+            this.MemoryStore.Size = new System.Drawing.Size(83, 69);
+            this.MemoryStore.TabIndex = 2;
+            this.MemoryStore.Text = "MS";
+            this.MemoryStore.UseVisualStyleBackColor = true;
+            // 
+            // number4
+            // 
+            this.number4.Location = new System.Drawing.Point(153, 270);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(68, 70);
+            this.number4.TabIndex = 5;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = true;
+            // 
+            // number7
+            // 
+            this.number7.Location = new System.Drawing.Point(153, 186);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(68, 69);
+            this.number7.TabIndex = 4;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = true;
+            // 
+            // number0
+            // 
+            this.number0.Location = new System.Drawing.Point(153, 438);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(68, 70);
+            this.number0.TabIndex = 7;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = true;
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(153, 353);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(68, 69);
+            this.number1.TabIndex = 6;
+            this.number1.Text = "1";
+            this.number1.UseVisualStyleBackColor = true;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(43, 51);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(520, 31);
+            this.txtResults.TabIndex = 16;
+            // 
+            // ChangeArithmeticSign
+            // 
+            this.ChangeArithmeticSign.Location = new System.Drawing.Point(238, 438);
+            this.ChangeArithmeticSign.Name = "ChangeArithmeticSign";
+            this.ChangeArithmeticSign.Size = new System.Drawing.Size(68, 70);
+            this.ChangeArithmeticSign.TabIndex = 20;
+            this.ChangeArithmeticSign.Text = "+/-";
+            this.ChangeArithmeticSign.UseVisualStyleBackColor = true;
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(238, 353);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(68, 69);
+            this.number2.TabIndex = 19;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = true;
+            // 
+            // number5
+            // 
+            this.number5.Location = new System.Drawing.Point(238, 270);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(68, 70);
+            this.number5.TabIndex = 18;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = true;
+            // 
+            // number8
+            // 
+            this.number8.Location = new System.Drawing.Point(238, 186);
+            this.number8.Name = "number8";
+            this.number8.Size = new System.Drawing.Size(68, 69);
+            this.number8.TabIndex = 17;
+            this.number8.Text = "8";
+            this.number8.UseVisualStyleBackColor = true;
+            // 
+            // Dot
+            // 
+            this.Dot.Location = new System.Drawing.Point(323, 438);
+            this.Dot.Name = "Dot";
+            this.Dot.Size = new System.Drawing.Size(68, 70);
+            this.Dot.TabIndex = 24;
+            this.Dot.Text = ".";
+            this.Dot.UseVisualStyleBackColor = true;
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(323, 353);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(68, 69);
+            this.number3.TabIndex = 23;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = true;
+            // 
+            // number6
+            // 
+            this.number6.Location = new System.Drawing.Point(323, 270);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(68, 70);
+            this.number6.TabIndex = 22;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = true;
+            // 
+            // number9
+            // 
+            this.number9.Location = new System.Drawing.Point(323, 186);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(68, 69);
+            this.number9.TabIndex = 21;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = true;
+            // 
+            // Addition
+            // 
+            this.Addition.Location = new System.Drawing.Point(410, 438);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(68, 70);
+            this.Addition.TabIndex = 28;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = true;
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(410, 353);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(68, 69);
+            this.Minus.TabIndex = 27;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(410, 270);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(68, 70);
+            this.Multiply.TabIndex = 26;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // Divide
+            // 
+            this.Divide.Location = new System.Drawing.Point(410, 186);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(68, 69);
+            this.Divide.TabIndex = 25;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            // 
+            // Equals
+            // 
+            this.Equals.Location = new System.Drawing.Point(495, 353);
+            this.Equals.Name = "Equals";
+            this.Equals.Size = new System.Drawing.Size(68, 155);
+            this.Equals.TabIndex = 31;
+            this.Equals.Text = "=";
+            this.Equals.UseVisualStyleBackColor = true;
+            // 
+            // OneDividedByCurrentEntry
+            // 
+            this.OneDividedByCurrentEntry.Location = new System.Drawing.Point(495, 270);
+            this.OneDividedByCurrentEntry.Name = "OneDividedByCurrentEntry";
+            this.OneDividedByCurrentEntry.Size = new System.Drawing.Size(68, 70);
+            this.OneDividedByCurrentEntry.TabIndex = 30;
+            this.OneDividedByCurrentEntry.Text = "1/X";
+            this.OneDividedByCurrentEntry.UseVisualStyleBackColor = true;
+            // 
+            // SquareRoot
+            // 
+            this.SquareRoot.Location = new System.Drawing.Point(495, 186);
+            this.SquareRoot.Name = "SquareRoot";
+            this.SquareRoot.Size = new System.Drawing.Size(68, 69);
+            this.SquareRoot.TabIndex = 29;
+            this.SquareRoot.Text = "sqrt";
+            this.SquareRoot.UseVisualStyleBackColor = true;
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(153, 115);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(159, 56);
+            this.Back.TabIndex = 32;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(323, 115);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(240, 56);
+            this.Clear.TabIndex = 33;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // MemoryDispl
+            // 
+            this.MemoryDispl.Location = new System.Drawing.Point(43, 140);
+            this.MemoryDispl.Name = "MemoryDispl";
+            this.MemoryDispl.Size = new System.Drawing.Size(83, 31);
+            this.MemoryDispl.TabIndex = 34;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 532);
+            this.Controls.Add(this.MemoryDispl);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Equals);
+            this.Controls.Add(this.OneDividedByCurrentEntry);
+            this.Controls.Add(this.SquareRoot);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Dot);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.ChangeArithmeticSign);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number8);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.number7);
+            this.Controls.Add(this.MemoryPlus);
+            this.Controls.Add(this.MemoryStore);
+            this.Controls.Add(this.MemoryRecall);
+            this.Controls.Add(this.MemoryClear);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MemoryClear;
+        private System.Windows.Forms.Button MemoryRecall;
+        private System.Windows.Forms.Button MemoryPlus;
+        private System.Windows.Forms.Button MemoryStore;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Button number0;
+        private System.Windows.Forms.Button number1;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button ChangeArithmeticSign;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number8;
+        private System.Windows.Forms.Button Dot;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Button Equals;
+        private System.Windows.Forms.Button OneDividedByCurrentEntry;
+        private System.Windows.Forms.Button SquareRoot;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox MemoryDispl;
+    }
+}
+
